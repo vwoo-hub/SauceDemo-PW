7f75f2e0648739800dbd17f1175bdd4bdeb2826d
Vincent Woo <vincentxwoo@gmail.com>
2025-04-16 19:18:59 -0700

    add more login tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -16,12 +16,20 @@ class TestLogin:
 
         assert 'inventory' in self.login_page.page.url
 
-
     def test_login_locked_out_user(self):
         error_message = 'Epic sadface: Sorry, this user has been locked out.'
 
         self.login_page.type_username_field('locked_out_user')
         self.login_page.type_password_field('secret_sauce')
+        self.login_page.tap_login_button()
+
+        expect(self.login_page.error_message_label).to_have_text(error_message)
+
+    def test_login_invalid_credentials(self):
+        error_message = 'Epic sadface: Username and password do not match any user in this service'
+
+        self.login_page.type_username_field('locked_out_user')
+        self.login_page.type_password_field('no_sauce')
         self.login_page.tap_login_button()
 
         expect(self.login_page.error_message_label).to_have_text(error_message)
